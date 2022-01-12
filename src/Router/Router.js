e9769edb45df74ee680fd9af5bedf46c3de897fa
robsonffsantos <robson.2501@hotmail.com>
2022-01-12 09:11:01 -0300

    alterações no router para commit

--- src/Router/Router.js
+++ src/Router/Router.js
@@ -1,17 +1,17 @@
 import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
-// import Home from '../Pages/Home'
-// import PageNotFound from '../Pages/PageNotFound'
-// import Login from '../Pages/Login'
-// import Register from '../Pages/Register'
-// import Details from '../Pages/Details'
+import Home from '../Pages/Home'
+import PageNotFound from '../Pages/PageNotFound'
+import Login from '../Pages/Login'
+import Register from '../Pages/Register'
+import Details from '../Pages/Details'
 import UserProfile from '../Pages/UserProfile'
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Switch>
-        {/* <Route exact path={'/'}>
+        <Route exact path={'/'}>
           <Home />
         </Route>
 
@@ -23,19 +23,18 @@ export default function Router() {
           <Register />
         </Route>
 
+        <Route exact path = {`details`}>
+          <Details />
+        </ Route>
 
-                <Route exact path = {`details`}>
-                    <Details />
-                </ Route>
+        <Route exact path = {'/profile'}>
+          <UserProfile />
+        </ Route>
 
-                <Route exact path = {'/profile'}>
-                    <Userprofile />
-                </ Route>
-
-
-        {/* <Route>
+        <Route>
           <PageNotFound />
-        </Route> */}
+        </Route>
+
       </Switch>
     </BrowserRouter>
   )
