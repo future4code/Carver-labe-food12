0bef63c47198325444fb732146cbc6b8e7b40e1f
Kahena Mansano <kahenamansanoalmeida@gmail.com>
2022-01-13 16:46:41 -0400

    Correção de conflito.

--- src/Router/Router.js
+++ src/Router/Router.js
@@ -1,40 +1,35 @@
 import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
-import Home from '../Pages/Home'
-import PageNotFound from '../Pages/PageNotFound'
-import Login from '../Pages/Login'
-import Register from '../Pages/Register'
-import Details from '../Pages/Details'
+// import Home from '../Pages/Home'
+// import PageNotFound from '../Pages/PageNotFound'
+// import Login from '../Pages/Login'
+// import Register from '../Pages/Register'
+// import Details from '../Pages/Details'
 import UserProfile from '../Pages/UserProfile'
-
 export default function Router() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path={'/'}>
+        {/* <Route exact path={'/'}>
           <Home />
         </Route>
-
         <Route exact path={'/login'}>
           <Login />
         </Route>
-
         <Route exact path={'/register'}>
           <Register />
         </Route>
-
-        <Route exact path = {`details`}>
+        <Route exact path={'/details'}>
           <Details />
-        </ Route>
+        </Route> */}
+        {/* <Route exact path={'/userprofile'}> */}
 
-        <Route exact path = {'/profile'}>
+        <Route exact path={'/'}>
           <UserProfile />
-        </ Route>
-
-        <Route>
-          <PageNotFound />
         </Route>
-
+        {/* <Route>
+          <PageNotFound />
+        </Route> */}
       </Switch>
     </BrowserRouter>
   )
